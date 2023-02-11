26e7d0f3b43fb89a7ae4df56f9da793d52b19b37
saveligulas <saveli.gulas@gmail.com>
2023-02-11 10:17:49 +0100

    Initial commit

---
+++ src/main/java/com/example/libraryFrontendAPI/LibraryFrontendApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryFrontendAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryFrontendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryFrontendApiApplication.class, args);
+	}
+
+}
